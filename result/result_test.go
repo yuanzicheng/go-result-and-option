d9e1bdbf09cb73c723d78e3765182c212a1398ae
Aioria <11429050@qq.com>
2024-03-05 22:08:27 +0800

    TestResult

--- result/result_test.go
+++ result/result_test.go
@@ -2,10 +2,15 @@ package result
 
 import "testing"
 
-func TestIsOk(t *testing.T) {
+func TestResult(t *testing.T) {
 	var x int = 12345
 	r := New(&x, nil)
 	if !r.IsOk() {
 		t.Error("IsOk failed")
+	}
+
+	v := r.Unwrap()
+	if v != x {
+		t.Error("Unwrap failed")
 	}
 }
